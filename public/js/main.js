4dba5a8601488eaa4ff9e2825758fb789a2e5b81
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-03-16 17:26:20 -0400

    ADD: FILES AND FOLDERS, AND SET ROUTERS

---
+++ public/js/main.js
@@ -0,0 +1,49 @@
+
+(() => {
+  const progressCircle = document.querySelector(".autoplay-progress svg"),
+        progressContent = document.querySelector(".autoplay-progress span");
+
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+      }
+    });
+
+  // Handle scrolling when tapping on the navbar menu
+  const navbarMenu = document.querySelector('#navbar__menu');
+
+  navbarMenu.addEventListener("click", (event) => {
+  let target = event.target,
+  link = target.dataset.link;
+  if(link == null) {
+  return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+  });
+
+  // Navbar toggle button for small screen
+  const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+  navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+  });
+
+
+})()
