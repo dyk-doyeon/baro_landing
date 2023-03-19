778e1d1047173a5923de683eea5d967bc675892f
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-03-19 19:56:48 -0400

    ADD: CHANGE FILES AND FOLDERS

--- routes/contract.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/contract', function (req, res, next) {
-  res.render('contract', { title: 'This is contract page' });
-});
-
-module.exports = router;
--- routes/introduce.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/introduce', function (req, res, next) {
-  res.render('introduce', { title: 'This is introduce page' });
-});
-
-module.exports = router;
