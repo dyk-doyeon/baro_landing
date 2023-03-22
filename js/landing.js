0f99f2a237b33f114ff5e52a7b2cae1e97daec9f
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-03-21 21:40:21 -0400

    BUILT: LANDING PAGE TABLET VERSION

---
+++ js/landing.js
@@ -0,0 +1,20 @@
+
+(() => {
+  const openButton = document.querySelector(".openBtn"),
+        navTabletContent = document.querySelector("#navbar__tablet"),
+        closeButton = document.querySelector(".closeBtn"),
+        originalNav = document.querySelector(".navBtn");
+
+  function openTabletNav() {
+    navTabletContent.style.display = "block";
+    originalNav.style.display = "none";
+  }
+
+  function closeTabletNav() {
+    navTabletContent.style.display = "none";
+    originalNav.style.display = "block";
+  }
+  
+  openButton.addEventListener("click", openTabletNav);
+  closeButton.addEventListener("click", closeTabletNav)
+})()
